4ac6fe8d7d7ea1e6b226db8095d8f69d26e5f20c
Natasha Pierre-Louis <nplouis@entricio.com>
2022-09-08 09:04:55 -0600

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# fem-tip-calculator-app-main
-Tip Calculator
+# Frontend Mentor - Tip calculator app
+
+This is a solution to the [Tip Calculator App challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### Screenshot of My Solution
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [https://github.com/natashapl/fem-tip-calculator-app-main](https://github.com/natashapl/fem-tip-calculator-app-main)
+- Live Site URL: [https://natashapl.github.io/fem-tip-calculator-app-main/](https://natashapl.github.io/fem-tip-calculator-app-main/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+
+### What I learned
+
+For this project, I decided to challenge myself to create the javaScript for this app without looking at anyone else's solution. After years of working with javaScript, I still haven't quite mastered it yet, so projects like these area great practice. I did use online resources such as W3 Schools and MDN Web Docs and the like, as needed.
+
+### Useful resources
+
+- [MDN Web Docs](https://developer.mozilla.org/) - A great resource for all things related to CSS, HTML and Javascript.
+- [W3 Schools](https://www.w3schools.com/) - Another great resource for all things related to CSS, HTML and Javascript
+- [20+ Amazing Pure CSS Accordions [Examples 2022]](https://alvarotrigo.com/blog/css-accordion/) - More examples of pure CSS accordions for inspiration.
+- [Name that Color](https://chir.ag/projects/name-that-color/) - This is still my favorite utility tool for generating unique color names from hex codes.
+
+## Author
+
+- Website - [Natasha Pierre-Louis](https://www.natashasfolio.com)
+- Frontend Mentor - [@natashapl](https://www.frontendmentor.io/profile/natashapl)
+- LinkedIn - [natashapl](https://www.linkedin.com/in/natashapl/)
---
+++ js/index.js
@@ -0,0 +1,173 @@
+/*
+ * Author: Natasha Pierre-Louis
+ *  Script for Tip Calculator - Frontend Mentor Challenge
+ */
+
+//Function that handles the calculations for this calculator
+const tipCalculator = (bill, tip, numOfPeople) => {
+  let convertedTip = tip / 100 || 0;
+  let tipAmount = bill * convertedTip || 0;
+  let totalBill = bill + tipAmount || 0;
+  let tipPerPerson = tipAmount / numOfPeople || 0;
+  let splitBill = totalBill / numOfPeople || 0;
+
+  if (numOfPeople === 0 || numOfPeople === null) {
+    tipPerPerson = 0;
+    splitBill = 0;
+  }
+
+  return {
+    bill: bill,
+    tip: tip,
+    numOfPeople: numOfPeople,
+    tipPerPerson: roundOff(tipPerPerson, 2),
+    tipAmount: roundOff(tipAmount, 2),
+    totalBill: roundOff(totalBill, 2),
+    splitBill: roundOff(splitBill, 2),
+  };
+};
+
+//Function that updates calculator everytime there's a change
+const updateCalculator = () => {
+  let billInput = document.querySelector(".billInput");
+  let tipInput = document.querySelectorAll(".tipInput");
+  let customInput = document.querySelector(".customInput");
+  let resetButton = document.querySelector(".resetButton");
+  let tipPerPerson = document.querySelector(".tipPerPerson");
+  let totalPerPerson = document.querySelector(".totalPerPerson");
+  let numOfPeopleInput = document.querySelector(".numOfPeopleInput");
+  let error = document.querySelector(".numOfPeopleSection .errorMessage");
+  let totalTip = 0;
+  let numOfPeopleTotal = 0;
+  let calculatorTotals = {};
+  const resetForm = () => {
+    resetButton.addEventListener("click", () => {
+      tipPerPerson.innerHTML = "$0.00";
+      totalPerPerson.innerHTML = "$0.00";
+
+      tipInput.forEach((elem) => {
+        elem.classList.remove("active");
+      });
+
+      setTimeout(() => {
+        resetButton.disabled = true;
+      }, 500);
+  
+    });
+  };
+
+  document.querySelector("main").addEventListener("keyup", (e) => {
+    let elem = e.target;
+
+    //Set Total Tip value
+    if (
+      parseInt(billInput.value) > 0 &&
+      elem.name === "customInput" &&
+      parseInt(elem.value) > 0
+    ) {
+      totalTip = parseInt(customInput.value);
+    }
+
+    //Set Number of People value
+    if (
+      parseInt(billInput.value) > 0 &&
+      elem.name === "numOfPeopleInput" &&
+      parseInt(elem.value) > 0
+    ) {
+      numOfPeopleTotal = parseInt(elem.value);
+    }
+
+    //Show/Hide validation error message
+    if (parseInt(numOfPeopleInput.value) === 0) {
+      numOfPeopleInput.classList.add("error");
+      error.style.display = "inline-block";
+    } else {
+      numOfPeopleInput.classList.remove("error");
+      error.style.display = "none";
+    }
+
+
+    calculatorTotals = tipCalculator(
+      parseInt(billInput.value),
+      totalTip,
+      numOfPeopleTotal
+    );
+
+    //Display Calculation Totals
+    for (let prop in calculatorTotals) {
+      tipPerPerson.innerHTML = formatCurrency(calculatorTotals.tipPerPerson);
+      totalPerPerson.innerHTML = formatCurrency(calculatorTotals.splitBill);
+    }
+
+    resetForm();
+
+    resetButton.disabled = false;
+  });
+
+  resetForm();
+
+  //Various events for the tip buttons
+  tipInput.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      let inputElem = e.target;
+
+      resetButton.disabled = false;
+
+      tipInput.forEach((otherButton) => otherButton.classList.remove("active"));
+      inputElem.classList.toggle("active");
+
+      if (parseInt(billInput.value) > 0) {
+        totalTip = parseInt(inputElem.value);
+        customInput.value = "";
+
+        calculatorTotals = tipCalculator(
+          parseInt(billInput.value),
+          totalTip,
+          numOfPeopleTotal
+        );
+
+        for (let prop in calculatorTotals) {
+          tipPerPerson.innerHTML = formatCurrency(
+            calculatorTotals.tipPerPerson
+          );
+          totalPerPerson.innerHTML = formatCurrency(calculatorTotals.splitBill);
+        }
+      }
+    });
+  });
+
+  //Removes active class from other tip button when Custom input field is clicked
+  customInput.addEventListener("click", (e) => {
+    tipInput.forEach((elem) => {
+      elem.classList.remove("active");
+    });
+  });
+
+  //Disables reset button if all input fields are empty and not in focus
+  document.querySelectorAll("input[type=number]").forEach((elem) => {
+      elem.addEventListener("blur", (e) => {
+          if(elem.value === "") {
+              resetButton.disabled = true;
+          }
+      });
+  })
+
+};
+
+//Function that rounds off dollar price
+const roundOff = (num, places) => {
+  const x = Math.pow(10, places);
+  return Math.round(num * x) / x;
+};
+
+//Function with third party script that formats currency fields.
+const formatCurrency = (num) => {
+  let dollars = new Intl.NumberFormat(`en-US`, {
+    currency: `USD`,
+    style: "currency",
+  }).format(num);
+
+  return dollars;
+};
+
+updateCalculator();
